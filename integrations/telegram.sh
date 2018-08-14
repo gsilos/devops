03bcaf467d76ac4426f3e625cbc9fc25888e1195
Fabiano Silos <gsilos@users.noreply.github.com>
2018-08-14 21:44:49 +0200

    example

--- integrations/telegram.sh
+++ integrations/telegram.sh
@@ -1,6 +1,11 @@
+#!/usr/bin/env bash
+
 function notify {
 	DST=123456789
 	BOTID=123456789:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 	MSG=$1
 	curl --connect-timeout 3 -s -o /dev/null https://api.telegram.org/bot${BOTID}/sendMessage?chat_id="$DST"\&text="$MSG"
 }
+
+
+notify "hello world"
